0f928fe0dea21cbcbbffa65c9a6302032f66c59b
deshpande.jaidev@gmail.com <deshpande.jaidev@gmail.com>
2026-03-09 18:00:53 +0530

    Remove the Reflection removal repo
    Too much for the workshop

--- Reflection_RemoVal_CVPR2024-main/networks/efficientnet_pytorch/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "0.7.1"
-from networks.efficientnet_pytorch.model import EfficientNet, VALID_MODELS
-from networks.efficientnet_pytorch.utils import (
-    GlobalParams,
-    BlockArgs,
-    BlockDecoder,
-    efficientnet,
-    get_model_params,
-)
