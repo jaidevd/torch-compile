1e6154a4002e11a2e3917f8e5b6a0dec832b0aaf
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2026-03-11 10:10:50 +0530

    SAM2: Use all torch modules except the predictor in the same script.
    Remove the use of functions to load and instantiate torch modules

--- sam2/inference.py
+++ sam2/inference.py
@@ -22,129 +22,126 @@ POINT_COORDS = np.array([[500, 375]], dtype=np.float32)
 POINT_LABELS = np.array([1], dtype=np.int32)
 
 
-def build_sam2(ckpt_path, device="cpu", mode="eval"):
-    # Image encoder
-    trunk = Hiera(
-        embed_dim=96,
-        num_heads=1,
-        stages=[1, 2, 7, 2],
-        global_att_blocks=[5, 7, 9],
-        window_pos_embed_bkg_spatial_size=[7, 7],
-    )
-    neck = FpnNeck(
-        position_encoding=PositionEmbeddingSine(
-            num_pos_feats=256,
-            normalize=True,
-            scale=None,
-            temperature=10000,
-        ),
-        d_model=256,
-        backbone_channel_list=[768, 384, 192, 96],
-        fpn_top_down_levels=[2, 3],
-        fpn_interp_model="nearest",
-    )
-    image_encoder = ImageEncoder(trunk=trunk, neck=neck, scalp=1)
+trunk = Hiera(
+    embed_dim=96,
+    num_heads=1,
+    stages=[1, 2, 7, 2],
+    global_att_blocks=[5, 7, 9],
+    window_pos_embed_bkg_spatial_size=[7, 7],
+)
+neck = FpnNeck(
+    position_encoding=PositionEmbeddingSine(
+        num_pos_feats=256,
+        normalize=True,
+        scale=None,
+        temperature=10000,
+    ),
+    d_model=256,
+    backbone_channel_list=[768, 384, 192, 96],
+    fpn_top_down_levels=[2, 3],
+    fpn_interp_model="nearest",
+)
+image_encoder = ImageEncoder(trunk=trunk, neck=neck, scalp=1)
 
-    # Memory attention
-    memory_attention = MemoryAttention(
-        d_model=256,
-        pos_enc_at_input=True,
-        layer=MemoryAttentionLayer(
-            activation="relu",
-            dim_feedforward=2048,
+# Memory attention
+memory_attention = MemoryAttention(
+    d_model=256,
+    pos_enc_at_input=True,
+    layer=MemoryAttentionLayer(
+        activation="relu",
+        dim_feedforward=2048,
+        dropout=0.1,
+        pos_enc_at_attn=False,
+        self_attention=RoPEAttention(
+            rope_theta=10000.0,
+            feat_sizes=[64, 64],
+            embedding_dim=256,
+            num_heads=1,
+            downsample_rate=1,
             dropout=0.1,
-            pos_enc_at_attn=False,
-            self_attention=RoPEAttention(
-                rope_theta=10000.0,
-                feat_sizes=[64, 64],
-                embedding_dim=256,
-                num_heads=1,
-                downsample_rate=1,
-                dropout=0.1,
-            ),
-            d_model=256,
-            pos_enc_at_cross_attn_keys=True,
-            pos_enc_at_cross_attn_queries=False,
-            cross_attention=RoPEAttention(
-                rope_theta=10000.0,
-                feat_sizes=[64, 64],
-                rope_k_repeat=True,
-                embedding_dim=256,
-                num_heads=1,
-                downsample_rate=1,
-                dropout=0.1,
-                kv_in_dim=64,
-            ),
         ),
-        num_layers=4,
-    )
+        d_model=256,
+        pos_enc_at_cross_attn_keys=True,
+        pos_enc_at_cross_attn_queries=False,
+        cross_attention=RoPEAttention(
+            rope_theta=10000.0,
+            feat_sizes=[64, 64],
+            rope_k_repeat=True,
+            embedding_dim=256,
+            num_heads=1,
+            downsample_rate=1,
+            dropout=0.1,
+            kv_in_dim=64,
+        ),
+    ),
+    num_layers=4,
+)
 
-    # Memory encoder
-    memory_encoder = MemoryEncoder(
-        out_dim=64,
-        position_encoding=PositionEmbeddingSine(
-            num_pos_feats=64,
-            normalize=True,
-            scale=None,
-            temperature=10000,
+# Memory encoder
+memory_encoder = MemoryEncoder(
+    out_dim=64,
+    position_encoding=PositionEmbeddingSine(
+        num_pos_feats=64,
+        normalize=True,
+        scale=None,
+        temperature=10000,
+    ),
+    mask_downsampler=MaskDownSampler(
+        kernel_size=3,
+        stride=2,
+        padding=1,
+    ),
+    fuser=Fuser(
+        layer=CXBlock(
+            dim=256,
+            kernel_size=7,
+            padding=3,
+            layer_scale_init_value=1e-6,
+            use_dwconv=True,
         ),
-        mask_downsampler=MaskDownSampler(
-            kernel_size=3,
-            stride=2,
-            padding=1,
-        ),
-        fuser=Fuser(
-            layer=CXBlock(
-                dim=256,
-                kernel_size=7,
-                padding=3,
-                layer_scale_init_value=1e-6,
-                use_dwconv=True,
-            ),
-            num_layers=2,
-        ),
-    )
+        num_layers=2,
+    ),
+)
 
-    # SAM2 model
-    model = SAM2Base(
-        image_encoder=image_encoder,
-        memory_attention=memory_attention,
-        memory_encoder=memory_encoder,
-        num_maskmem=7,
-        image_size=1024,
-        sigmoid_scale_for_mem_enc=20.0,
-        sigmoid_bias_for_mem_enc=-10.0,
-        use_mask_input_as_output_without_sam=True,
-        directly_add_no_mem_embed=True,
-        no_obj_embed_spatial=True,
-        use_high_res_features_in_sam=True,
-        multimask_output_in_sam=True,
-        iou_prediction_use_sigmoid=True,
-        use_obj_ptrs_in_encoder=True,
-        add_tpos_enc_to_obj_ptrs=True,
-        proj_tpos_enc_in_obj_ptrs=True,
-        use_signed_tpos_enc_to_obj_ptrs=True,
-        only_obj_ptrs_in_the_past_for_eval=True,
-        pred_obj_scores=True,
-        pred_obj_scores_mlp=True,
-        fixed_no_obj_ptr=True,
-        multimask_output_for_tracking=True,
-        use_multimask_token_for_obj_ptr=True,
-        multimask_min_pt_num=0,
-        multimask_max_pt_num=1,
-        use_mlp_for_obj_ptr_proj=True,
-        compile_image_encoder=False,
-        sam_mask_decoder_extra_args={
-            "dynamic_multimask_via_stability": True,
-            "dynamic_multimask_stability_delta": 0.05,
-            "dynamic_multimask_stability_thresh": 0.98,
-        },
-    )
-    sd = torch.load(ckpt_path, map_location="cpu", weights_only=True)["model"]
-    missing_keys, unexpected_keys = model.load_state_dict(sd)
+# SAM2 model
+model = SAM2Base(
+    image_encoder=image_encoder,
+    memory_attention=memory_attention,
+    memory_encoder=memory_encoder,
+    num_maskmem=7,
+    image_size=1024,
+    sigmoid_scale_for_mem_enc=20.0,
+    sigmoid_bias_for_mem_enc=-10.0,
+    use_mask_input_as_output_without_sam=True,
+    directly_add_no_mem_embed=True,
+    no_obj_embed_spatial=True,
+    use_high_res_features_in_sam=True,
+    multimask_output_in_sam=True,
+    iou_prediction_use_sigmoid=True,
+    use_obj_ptrs_in_encoder=True,
+    add_tpos_enc_to_obj_ptrs=True,
+    proj_tpos_enc_in_obj_ptrs=True,
+    use_signed_tpos_enc_to_obj_ptrs=True,
+    only_obj_ptrs_in_the_past_for_eval=True,
+    pred_obj_scores=True,
+    pred_obj_scores_mlp=True,
+    fixed_no_obj_ptr=True,
+    multimask_output_for_tracking=True,
+    use_multimask_token_for_obj_ptr=True,
+    multimask_min_pt_num=0,
+    multimask_max_pt_num=1,
+    use_mlp_for_obj_ptr_proj=True,
+    compile_image_encoder=False,
+    sam_mask_decoder_extra_args={
+        "dynamic_multimask_via_stability": True,
+        "dynamic_multimask_stability_delta": 0.05,
+        "dynamic_multimask_stability_thresh": 0.98,
+    },
+)
+sd = torch.load(CHECKPOINT, map_location="cpu", weights_only=True)["model"]
+missing_keys, unexpected_keys = model.load_state_dict(sd)
 
-    model = model.to(device)
-    return model.eval()
+model = model.to("cpu").eval()
 
 
 def show_mask(ax, mask, alpha=0.5):
@@ -157,9 +154,7 @@ def show_mask(ax, mask, alpha=0.5):
 image = np.array(Image.open(IMAGE_PATH).convert("RGB"))
 
 
-predictor = SAM2ImagePredictor(
-    build_sam2(ckpt_path=CHECKPOINT, device="cpu", mode="eval")
-)
+predictor = SAM2ImagePredictor(model)
 with torch.no_grad():
     predictor.set_image(image)
     masks, ious, _ = predictor.predict(
